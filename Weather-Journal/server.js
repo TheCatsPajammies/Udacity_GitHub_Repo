f08270a89d96a478896c9c0bd08c4449cb3421ca
TheCatsPajammies <67980898+TheCatsPajammies@users.noreply.github.com>
2021-01-01 20:50:50 -0600

    Testing/Debugging

--- Weather-Journal/server.js
+++ Weather-Journal/server.js
@@ -1,53 +1,49 @@
-// express, name, body-parser, and cors installed into project directory - 12/18/2020
-
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
 
+// Dependencies
+const bodyParser = require('body-parser');
+
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
-// const { request } = require('http');
-// const { response } = require('express');
-// const { getPackedSettings } = require('http2');
-
-
 // Initialize the main project folder
 app.use(express.static('website'));
-const port = 3000;
 
+// Post Route
+const data = [];
+app.post('/add', addData);
+
+function addData(req, res) {
+  projectData['date'] = req.body.date;
+  projectData['temp'] = req.body.temp;
+  projectData['content'] = req.body.content;
+  res.send(projectData);
+}
+
+// Callback function to complete GET '/all'
+app.get('/all', getData);
+
+function getData(req, res) {
+  res.send(projectData);
+}
 
 // Setup Server
+const port = 3000;
 const server = app.listen(port, listening);
-function listening(){
+
+function listening() {
     console.log("The Weather-Journal Server is running, my dudes!"); 
     console.log(`Using localhost: ${port}`);
-};
-
-// array to hold data;
-const data = [];
-// Routes
-// Get Route pulls projectData and 
-app.get('/all', function (req, res) {
-    res.send(projectData);
-    console.log("projectData");
-    console.log("get");
-});
-
-// Post Route adds data to projectData
-app.post('/addWeather', addWeather);
-
-function addWeather (req, res) {
-    data.push(req.body);
-    console.log(data);
 };
--- Weather-Journal/website/app.js
+++ Weather-Journal/website/app.js
@@ -1,59 +1,84 @@
 /* Global Variables */
-// Create a new date instance dynamically with JS
+
+// Base URL and API Key for OpenWeatherMap API
+const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=7df24cf86819600a46e06c0ae48b5484';
+
+//Get the date
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = String((d.getMonth() + 1)).padStart(2, "0") + '/' + String(d.getDate()).padStart(2, "0") + '/' + d.getFullYear() + ' @ ' + String(d.getHours()).padStart(2, "0") + ':' + String(d.getMinutes()).padStart(2, "0");
 
-// API url and key for openweathermap.org
-// Complete call format is going to be (weatherURL + zipCode + apiKey).
-// zipCode variable is set in the GET request for weather.
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
 
-const weatherURL = `http://api.openweathermap.org/data/2.5/weather?zip=`;
-const apiKey = ',us&appid=7df24cf86819600a46e06c0ae48b5484';
+/* Function called by event listener */
+function performAction(e) {
+  e.preventDefault();
+  // get user input values
+  const zipCode = document.getElementById('zip').value;
+  const content = document.getElementById('feelings').value;
 
-document.getElementById('generate').addEventListener('click', apiCall);
+  getWeather(weatherURL, zipCode, apiKey)
+    .then(function (clientData) {
+      // add data to POST request
+      console.log(clientData)
+      postData('/add', { date: newDate, temp: clientData.main.temp, content })
+    }).then((newData) => {
+        // call updateUI to update browser content
+      updateUI();
+    })
+}
 
-function apiCall(e) {
-    const zipCode = document.getElementById('zip').value;
-    getAPI(weatherURL, zipCode, apiKey);
+/* Function to GET Web API Data*/
+const getWeather = async (weatherURL, zipCode, apiKey) => {
+  // res equals the result of fetch function
+  const res = await fetch(weatherURL + zipCode + apiKey);
+  try {
+    // clientData equals the result of fetch function
+    const clientData = await res.json();
+    return clientData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  const req = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8"
+    },
+    body: JSON.stringify({
+      date: data.date,
+      temp: `${data.temp} K, ${(data.temp - 273.15).toFixed(2)} C, ${((data.temp - 273.15) * (9/5) + 32).toFixed(2)} F`,
+      content: data.content
+    })
+  })
+
+  try {
+    const newData = await req.json();
+    return newData;
+  }
+  catch (error) {
+    console.log(error);
+  }
 };
 
-const getAPI = async (weatherURL, zipCode, apiKey) => {
-    const res = await fetch(weatherURL + zipCode + apiKey)
-    try {
-        const data = await res.json();
-        console.log(data);
-        console.log(newDate, d);
-        return data;
-    }   
-    catch(error) {
-        console.log(`error ${weatherURL}${zipCode}${apiKey}`, error);
-    }
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json()
+    // updates the new entry values
+    document.getElementById('date').innerHTML = allData.date;
+    document.getElementById('temp').innerHTML = allData.temp;
+    document.getElementById('content').innerHTML = allData.content;
+  }
+  catch (error) {
+    console.log("error", error);
+  }
 };
-
-
-const postData = async ( url = '', data = {})=>{
-    console.log(data);
-      const response = await fetch(url, {
-      method: 'POST', 
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-     // Body data type must match "Content-Type" header        
-      body: JSON.stringify(data), 
-    });
-
-      try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-      }catch(error) {
-      console.log("error", error);
-    }
-};
-
-postData("/addWeather", {temp: 45, conditions: "windy"});
-postData("/addWeather", {temp: 65, conditions: "raining"});
-
 
 
