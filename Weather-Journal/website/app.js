1805b388a9b9821571c451b3449c835b48cbb513
TheCatsPajammies <67980898+TheCatsPajammies@users.noreply.github.com>
2021-01-04 11:13:25 -0600

    Updated JSON packages/edited for review/submission

--- Weather-Journal/website/app.js
+++ Weather-Journal/website/app.js
@@ -2,12 +2,11 @@
 
 // Base URL and API Key for OpenWeatherMap API
 const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '&appid=7df24cf86819600a46e06c0ae48b5484';
+const apiKey = '&appid=7df24cf86819600a46e06c0ae48b5484&units=metric';
 
 //Get the date
-// I commented out the default date because it doesn't refresh in session - any reason for that?
-// let d = new Date();
-// let newDate = String((d.getMonth() + 1)).padStart(2, "0") + '/' + String(d.getDate()).padStart(2, "0") + '/' + d.getFullYear() + ' @ ' + String(d.getHours()).padStart(2, "0") + ':' + String(d.getMinutes()).padStart(2, "0");
+let d = new Date();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
@@ -26,11 +25,11 @@ function performAction(e) {
   getWeather(weatherURL, zipCode, apiKey)
     .then(function (clientData) {
       console.log(clientData);
-      console.log(Date());
-      postData('/add', { date: Date(), temp: clientData.main.temp, content })
-    }).then(() => {
-        updateUI();
-      })
+      console.log(newDate);
+      postData('/add', { date: newDate, temp: clientData.main.temp, content })
+    }).then(function (tempData) {
+      updateUI();
+    })
 }
 
 /* Function to GET Web API Data*/
@@ -56,7 +55,7 @@ const postData = async (url = '', data = {}) => {
     },
     body: JSON.stringify({
       date: data.date,
-      temp: `${data.temp} K, ${(data.temp - 273.15).toFixed(2)} C, ${((data.temp - 273.15) * (9/5) + 32).toFixed(2)} F`,
+      temp: data.temp,
       content: data.content
     })
   })
@@ -75,7 +74,7 @@ const updateUI = async () => {
   try {
     const entryData = await request.json()
     document.getElementById('date').innerHTML = entryData.date;
-    document.getElementById('temp').innerHTML = entryData.temp;
+    document.getElementById('temp').innerHTML = entryData.temp + " Celsius";
     document.getElementById('content').innerHTML = entryData.content;
   }
   catch (error) {
